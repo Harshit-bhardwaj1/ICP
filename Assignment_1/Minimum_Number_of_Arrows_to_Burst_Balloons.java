b1a1936327b21587dc3758ad809127e1f7ec0259
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-09-27 02:20:48 +0530

    Add files via upload

---
+++ Assignment_1/Minimum_Number_of_Arrows_to_Burst_Balloons.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class Minimum_Number_of_Arrows_to_Burst_Balloons {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[][] = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+        System.out.println(MinArrow(arr));
+    }
+    public static int MinArrow(int[][] arr){
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int arrow=1;
+        int arrowpos= arr[0][1];
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i][0]> arrowpos){
+                arrow++;
+                arrowpos=arr[i][1];
+            }
+        }
+        return arrow;
+    }
+}
